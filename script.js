ba3189dc80b52512088d6985aa3615eda9bff61d
durand1911 <khryu7134@gmail.com>
2024-07-30 12:50:06 -0400

    Create getHumanChoice function to get the human choice from input

--- script.js
+++ script.js
@@ -3,16 +3,27 @@
 // 1) Create a new function named getComputerChoice
 // 2) Write the code so that getComputerChoice function will randomly return one of three options => rock, paper, scissors
 function getComputerChoice() {
-    let result = 0;
-    result = Math.floor(Math.random() * 3) + 1;
+    let comChoice = 0;
+    comChoice = Math.floor(Math.random() * 3) + 1;
     
-    if(result === 1) {
+    if(comChoice === 1) {
         return "rock";
-    } else if(result === 2) {
+    } else if(comChoice === 2) {
         return "paper";
-    } else if(result === 3) {
+    } else if(comChoice === 3) {
         return "scissors";
     }
 }
 // 3) Test that my function returns what I expect
 console.log(getComputerChoice());
+
+// 2. Write the logic to get the human choice
+// 1) Create a new function named getHumanChoice
+// 2) Write the code so that the function will return one of three options based on what the user inputs
+
+function getHumanChoice() {
+    let humanChoice = prompt("Please enter your choice: ");
+    return humanChoice;
+}
+
+console.log(getHumanChoice());
